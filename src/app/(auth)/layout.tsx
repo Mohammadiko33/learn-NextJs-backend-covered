2ae0a6b6dfdbcc6e428fb381b69fe024b1961284
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-06 19:42:32 +0330

    Next.js 15 Tutorial - 16 - Multiple Root Layouts

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className='rsc'>this is ({children}) children</div>
+  );
+};
+
+export default Layout;
