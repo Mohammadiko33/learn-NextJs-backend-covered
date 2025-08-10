fe0f1a441353a31d8752a66ae1231ee7f61387af
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-10 10:42:32 +0330

    46 - Middleware Part 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,11 @@ import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import "./globals.css";
 
-const episode = "45";
+const episode = "46";
 
 export const metadata: Metadata = {
   title: {
-    default: `Next.js - ${episode} - Caching in Route Handlers`,
+    default: `Next.js - ${episode} - Middleware`,
     template: `${episode} - %s`,
   },
   description: "Next.js v.15 Tutorial",
