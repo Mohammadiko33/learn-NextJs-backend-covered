4d6d860a090c912c9267321559965a26d0bae1fb
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-10 16:45:03 +0330

    49  - server side rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,11 @@ import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import "./globals.css";
 
-const episode = "47";
+const episode = "49";
 
 export const metadata: Metadata = {
   title: {
-    default: `Next.js - ${episode} - Rendering`,
+    default: `Next.js - ${episode} - server side rendering`,
     template: `${episode} - %s`,
   },
   description: "Next.js v.15 Tutorial",
