fbd10a935f0fbc5b0e5b18ee537a1714294ce73c
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-10 17:08:04 +0330

    50 - Suspense SSR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,11 @@ import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import "./globals.css";
 
-const episode = "49";
+const episode = "50";
 
 export const metadata: Metadata = {
   title: {
-    default: `Next.js - ${episode} - server side rendering`,
+    default: `Next.js - ${episode} - Suspense SSR`,
     template: `${episode} - %s`,
   },
   description: "Next.js v.15 Tutorial",
