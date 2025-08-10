ae815bc73a158cd49472e4142af987441aaada1d
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-10 10:44:52 +0330

    46 - Middleware Part 1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest){
-    const res = NextResponse.next()
-
-
-    return res
+export function middleware(req: NextRequest) {
+  return NextResponse.redirect(new URL("/", req.url));
 }
+
+export const config = {
+  matcher: "/profile",
+};
