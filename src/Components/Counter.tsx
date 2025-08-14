a0556d2297cf9007d1210f952a533cca60e332f6
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-14 13:17:32 +0330

    89 - Read Session and User Data Part 2

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -5,10 +5,10 @@ import { useAuth , useUser } from "@clerk/nextjs"
 const Counter = () => {
     const [count , setCount] = useState<number>(0)
 
-    const { isLoaded, userId, sessionId, getToken } = useAuth()
-    // const { isLoaded, isSignedIn, user } = useUser()
+    // const { isLoaded, userId, sessionId, getToken } = useAuth()
+    const { isLoaded, isSignedIn, user } = useUser()
 
-    if (!isLoaded || !userId) return null
+    if (!isLoaded || !isSignedIn) return null
 
   return <button className='bg-blue-400' onClick={() => setCount(prev => prev + 1)}>Count: {count}</button>
 }
