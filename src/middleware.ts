2915bd52e5609d7dee319188f43c3cb9c293e045
developer-iko-mike <developer.iko.mike@gmail.com>
2025-08-14 12:13:13 +0330

    88 - Protecting Routes Part 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { clerkMiddleware , createRouteMatcher} from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(["/user-profile"])
-// const isPublicRoute = createRouteMatcher(["/" , "/sign-in(.*)" , "/sign-out(.*)"])
+// const isProtectedRoute = createRouteMatcher(["/user-profile"])
+const isPublicRoute = createRouteMatcher(["/" , "/sign-in(.*)" , "/sign-out(.*)"])
 
 export default clerkMiddleware(async (auth , req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  if (!isPublicRoute(req)) await auth.protect();
 });
 
 export const config = {
